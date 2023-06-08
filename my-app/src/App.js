d29b80754d0467d6d9ebbb67285ad5e822128dad
joywhy <leh9111@naver.com>
2023-06-08 17:15:34 +0900

    Style: 가독성을 위한 기본 스타일링

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,18 @@
 import './App.css';
-
+import galssUrl from './glass.png';
+import styled from 'styled-components';
+const Container = styled.div`
+  width: 100%;
+  height: 500px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid red;
+`;
+const Galss = styled.img`
+  width: 100px;
+  height: auto;
+`;
 function App() {
   window.addEventListener(
     'scroll',
@@ -12,7 +25,11 @@ function App() {
     false,
   );
 
-  return <div className="App"></div>;
+  return (
+    <Container>
+      <Galss src={galssUrl} alt="잔" />
+    </Container>
+  );
 }
 
 export default App;
