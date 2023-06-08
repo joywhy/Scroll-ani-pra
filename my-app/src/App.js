38a9ba378defdcfafa4f4d94d39f77a789e0cf0d
joywhy <leh9111@naver.com>
2023-06-08 17:36:40 +0900

    Init: 세팅

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,30 +1,40 @@
 import './App.css';
 import galssUrl from './glass.png';
 import styled from 'styled-components';
+import React, { useEffect } from 'react';
+
+// window.addEventListener(
+//   'scroll',
+//   () => {
+//     document.body.style.setProperty(
+//       '--scroll',
+//       window.pageYOffset / (document.body.offsetHeight - window.innerHeight),
+//     );
+//   },
+//   false,
+// );
+
 const Container = styled.div`
   width: 100%;
-  height: 500px;
+  height: 1000px;
   display: flex;
   align-items: center;
   justify-content: center;
   border: 1px solid red;
+  box-sizing: border-box;
 `;
 const Galss = styled.img`
   width: 100px;
   height: auto;
+  border: 1px solid red;
+  position: fixed;
+  display: block;
+  animation: rotate 1s linear infinite;
+  animation-play-state: paused;
+  animation-delay: calc(var(--scroll) * -1s);
 `;
 function App() {
-  window.addEventListener(
-    'scroll',
-    () => {
-      document.body.style.setProperty(
-        '--scroll',
-        window.pageYOffset / (document.body.offsetHeight - window.innerHeight),
-      );
-    },
-    false,
-  );
-
+  useEffect(() => {});
   return (
     <Container>
       <Galss src={galssUrl} alt="잔" />
