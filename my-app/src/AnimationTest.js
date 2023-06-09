0cd8d2ecfd342d04b839bd8b9d28d207d7e1544d
joywhy <leh9111@naver.com>
2023-06-10 00:38:07 +0900

    style: 슬라이드 props배경  주석

--- my-app/src/AnimationTest.js
+++ my-app/src/AnimationTest.js
@@ -8,6 +8,9 @@ const Wrapper = styled.div`
   padding: 1.6rem;
   background: ${(props) =>
     props.backgroundColor ? props.backgroundColor : 'white'};
+  //적용안됨 왜?
+
+  background-color: gray;
   border: 1px solid rebeccapurple;
 `;
 
