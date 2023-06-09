a73e963471df82eb60c435709ac1e034a475a626
joywhy <leh9111@naver.com>
2023-06-09 23:39:52 +0900

    Style: 슬라이드 배경이미지 구현시도

--- my-app/src/AnimationTest.js
+++ my-app/src/AnimationTest.js
@@ -3,6 +3,28 @@ import styled, { keyframes } from 'styled-components';
 import { useWindowScrollEvent } from './hooks/useWindowScrollEvent';
 import { checkIsInViewport } from './utils/checkIsInViewport';
 import ScrollRevealSlideAnimation from './ScrollRevealSlideAnimation';
+const Wrapper = styled.div`
+  height: 40rem;
+  padding: 1.6rem;
+  background: ${(props) =>
+    props.backgroundColor ? props.backgroundColor : 'white'};
+  border: 1px solid rebeccapurple;
+`;
+
+//애니에 관한 css
+const goup = keyframes`
+  from { transform: translateY(5rem); opacity: 0; }
+  to { transform: translateY(0); opacity: 1; }
+`;
+
+const Text = styled.p`
+  //애니메이션 안해야할 때
+  ${({ animation }) => !animation && 'transform: translateY(5rem); opacity: 0;'}
+  //애니메이션 해야할 때 
+  animation: ${({ animation }) => animation && goup} 2s ease-out;
+  font-weight: bold;
+  font-size: 2rem;
+`;
 
 function AnimationTest() {
   const [animation, setAnimation] = useState(false);
@@ -28,23 +50,3 @@ function AnimationTest() {
 }
 
 export default AnimationTest;
-
-const Wrapper = styled.div`
-  height: 40rem;
-  padding: 1.6rem;
-`;
-
-//애니에 관한 css
-const goup = keyframes`
-  from { transform: translateY(5rem); opacity: 0; }
-  to { transform: translateY(0); opacity: 1; }
-`;
-
-const Text = styled.p`
-  //애니메이션 안해야할 때
-  ${({ animation }) => !animation && 'transform: translateY(5rem); opacity: 0;'}
-  //애니메이션 해야할 때 
-  animation: ${({ animation }) => animation && goup} 2s ease-out;
-  font-weight: bold;
-  font-size: 2rem;
-`;
